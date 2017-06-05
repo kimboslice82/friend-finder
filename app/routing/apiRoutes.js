145d07f008f203de34555f14aac9de7a8e94f899
Kimberley Hood <KimberleyHood@Kimberleys-MacBook-Pro.local>
2017-06-04 23:13:24 -0500

    update

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,22 +1,16 @@
-var friends = require("../data/friends");
+var friendList = require("../data/friends");
 
 module.exports = function(app) {
 
     app.get("/api/friends", function(req, res) {
-    res.json(friends);
+    res.json(friendList);
   });
 
     app.post("/api/friends", function(req, res) {
-    // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
-    // It will do this by sending out the value "true" have a table
-    /*if (tableData.length < 5) {
-      tableData.push(req.body);
-      res.json(true);
-    }
-    else {
-      waitListData.push(req.body);
-      res.json(false);
-    }*/
+     friendList.push(req.body);
+
+    
+
   });
 
 }
